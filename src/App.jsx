d1e18630a96e74afc65b6c372f4ec283e410c8d2
rote1400 <rote1400@student.miun.se>
2023-11-29 19:40:20 +0100

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
 import "./styles.css"
 
 export default function App() {
-  return "Hello world"
+  return (
+    <div>
+      <form className="new-note-form">
+        <div className="form-row">
+          <label htmlFor="note">New note</label>
+          <input type="text" id="note" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" /> Note 1
+          </label>
+          <button className="btn btn-danger">
+            Delete
+          </button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" /> Note 2
+          </label>
+          <button className="btn btn-danger">
+            Delete
+          </button>
+        </li>
+      </ul>
+    </div>
+  )
 }
