297ddd65762ceccceb5cd7bb22ee0b8c64f12565
rote1400 <rote1400@student.miun.se>
2023-11-30 14:49:31 +0100

    divided in components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 import { useState } from "react"
+import { NewNoteForm } from "./NewNoteForm"
+import { NotesList } from "./NotesList"
 import "./styles.css"
 
 export default function App() {
-  const [newNote, setNewNote] = useState("")
   const [notes, setNotes] = useState([])
 
-  function handleSubmitBtn(e) {
-    e.preventDefault()
-
+  function addNote(title) {
     setNotes(currentNotes => {
       return [
-        ...currentNotes,
-        { id: crypto.randomUUID(), title: newNote, completed: false},
+          ...currentNotes,
+          { id: crypto.randomUUID(), title, completed: false},
       ]
-    })
-
-    setNewNote("")
+      })
   }
 
   function toggleNote(id, completed) {
@@ -37,42 +34,13 @@ export default function App() {
   
   return (
     <div>
-      <form onSubmit={handleSubmitBtn} className="new-note-form">
-        <div className="form-row">
-          <label htmlFor="note">New note</label>
-          <input 
-            value={newNote}
-            onChange={e => setNewNote(e.target.value)}
-            type="text" 
-            id="note" 
-          />
-        </div>
-        <button className="btn">Add</button>
-      </form>
+      <NewNoteForm onAdd={addNote} />
       <h1 className="header">Todo List</h1>
-      <ul className="list">
-        {notes.length === 0 && "Empty"}
-        {notes.map(note => {
-          return (
-            <li key={note.id}>
-              <label>
-                <input 
-                  type="checkbox"
-                  checked={note.completed}
-                  onChange={e => toggleNote(note.id, e.target.checked)} 
-                />
-                {note.title}
-              </label>
-              <button 
-                className="btn btn-danger" 
-                onClick={() => deleteNote(note.id)}
-              >
-                Delete
-              </button>
-            </li>
-          )
-        })}
-      </ul>
+      <NotesList 
+        notes = {notes} 
+        toggleNote={toggleNote}
+        deleteNote={deleteNote} 
+      />
     </div>
   )
 }
